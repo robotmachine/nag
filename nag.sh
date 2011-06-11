0e4f799b2d9dfa38bb0f099fb68d97246ff6b85b
Brian <robotmachine@gmail.com>
2011-06-11 08:34:42 -0700

    Changed no parameter statement to show latest nag. Usaged moved to else.

--- nag.sh
+++ nag.sh
@@ -4,8 +4,8 @@
 # Using the basic structure outlined here: http://onethingwell.org/post/4441662022/nag-ii-unix-boogaloo
 # --
 #
-# If no parameter is entered, usage info will display.
-[ $# -eq 0 ] && { echo "Usage: nag ls, rm, add, clr, 1"; exit 1; }
+# If no parameter is entered, the latest nag will show.
+ [ $# -eq 0 ] && { head -1 ~/.nag; exit 1; }
 
 nag=$1
 rav=$2
@@ -20,12 +20,10 @@ elif [ $nag = "rm" ]; then
 	nagrm $rav
 elif [ $nag = "add" ]; then
 	echo $rav >> ~/.nag
-elif [ $nag = "1" ]; then
-	head -1 ~/.nag
 elif [ $nag = "clr" ]; then
 	echo "This will clear your nag file."
 	read -p "Press ctrl+c to cancel or any other key to continue."
 	true > ~/.nag
 else
-	echo "$nag is not a command. Try just typing nag for usage info."
+	echo "$nag is not a command. Usage: nag [ ls | add | rm # | clr ]"
 fi
